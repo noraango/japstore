0b8d662658b95312cb035b6456363b96aa59c570
VietTQ1411 <80516853+Tungdz2k2k@users.noreply.github.com>
2021-11-29 00:03:23 +0700

    update

--- src/components/common/Payment/ProductItem.js
+++ src/components/common/Payment/ProductItem.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-export default function ProductItem(product) {
+export default function ProductItem({ productItem }) {
   return (
     <div className="row cart-product">
       <div className="col-2">
         <img src={process.env.PUBLIC_URL + "/images/user.png"} alt="" />
       </div>
       <div className="col-6 cart-body">
-        <span>{product.name}</span>
+        <span>{productItem.name}</span>
       </div>
       <div className="col-4 cart-body">
-        <p>Số lượng : {product.quantity}</p>
-        <p>{product.price}</p>
+        <p>Số lượng : {productItem.quantity}</p>
+        <p>{productItem.price}</p>
       </div>
     </div>
   );
